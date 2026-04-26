be1c0dbcd96c40db568318f10bccdfeca413292e
Alina Komerysta <alinakomerista3@gmail.com>
2026-04-26 08:40:17 +0200

    change name props

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,25 +1,38 @@
-import { type Votes } from '../../types/votes';
-import styles from './VoteStats.module.css';
+import { type Votes } from "../../types/votes";
+import styles from "./VoteStats.module.css";
 
 interface VoteStatsProps {
   votes: Votes;
   totalVotes: number;
-  positivePercentage: number;
+  positiveRate: number;
 }
 
-export const VoteStats = ({ votes, totalVotes, positivePercentage }: VoteStatsProps) => {
+export const VoteStats = ({
+  votes,
+  totalVotes,
+  positiveRate,
+}: VoteStatsProps) => {
   const { good, neutral, bad } = votes;
-  
+
   return (
     <div>
       <div className={styles.container}>
-  <p className={styles.stat}>Good: <strong>{good}</strong></p>
-  <p className={styles.stat}>Neutral: <strong>{neutral}</strong></p>
-  <p className={styles.stat}>Bad: <strong>{bad}</strong></p>
-  <p className={styles.stat}>Total: <strong>{totalVotes}</strong></p>
-  <p className={styles.stat}>Positive: <strong>{positivePercentage}%</strong></p>
-</div>
-
+        <p className={styles.stat}>
+          Good: <strong>{good}</strong>
+        </p>
+        <p className={styles.stat}>
+          Neutral: <strong>{neutral}</strong>
+        </p>
+        <p className={styles.stat}>
+          Bad: <strong>{bad}</strong>
+        </p>
+        <p className={styles.stat}>
+          Total: <strong>{totalVotes}</strong>
+        </p>
+        <p className={styles.stat}>
+          positiveRateve: <strong>{positiveRate}%</strong>
+        </p>
+      </div>
     </div>
   );
 };
